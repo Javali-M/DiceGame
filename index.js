82448670e80f152af215db3ab1c742a5a0ae9106
Javali <javalimurari@gmail.com>
2022-06-24 21:14:13 +0530

    changes

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ var randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomdicesrc2 = "images/"+"dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomdicesrc2);
 if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="Congrats! Player 1 wins."
+    document.querySelector("h1").innerHTML="Congrats! Player 1 wins.";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Congrats! Player 2 wins.";
 }
 else{
-    document.querySelector("h1").innerHTML="Congrats! Player 2 wins."
+    document.querySelector("h1").innerHTML="It's a draw!!";
 }
